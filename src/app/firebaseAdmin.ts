afa87a8fbe8083763b006697a0787f4e739a8b6a
xrazz <rajtripathi2580@gmail.com>
2024-10-26 11:25:11 +0530

    testing firebaseAdmin.ts

--- src/app/firebaseAdmin.ts
+++ src/app/firebaseAdmin.ts
@@ -1,6 +1,6 @@
 // lib/firebaseAdmin.ts
 import * as admin from 'firebase-admin';
-
+console.log("FIREBASE client:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
 console.log("FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
 console.log("FIREBASE_PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY ? "[HIDDEN]" : "NOT FOUND");
