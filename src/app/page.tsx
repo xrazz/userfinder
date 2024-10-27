933eb7a370b83bf9c52cd73f38524c359a27e0ef
xrazz <rajtripathi2580@gmail.com>
2024-10-27 22:47:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,8 @@ export default function LandingPage() {
           <CallToAction />
         </main>
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-          <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 UserFinder AI. All rights reserved.</p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 UserFinder AI by Qunaternity Technologies Private Limited All rights reserved.</p>
+         <p className="text-xs text-gray-500 dark:text-gray-400">CIN - U62013MP2024PTC069972</p>
           <nav className="sm:ml-auto flex gap-4 sm:gap-6">
             <a className="text-xs hover:underline underline-offset-4" href="/refund">
               Refund Policy
