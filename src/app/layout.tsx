5a550e943b6b583f682614951bb7b8e342e920b5
Denis Cartin <cartin.denis@gmail.com>
2025-01-11 00:26:54 +0100

    analitycs enhanced: source tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Theme } from '@radix-ui/themes';
 import ClientProvider from './providers/ClientProvider';
 import { ThemeProvider } from './providers/ThemeProvider';
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -46,6 +47,18 @@ export default function RootLayout({
           title="Lexy Search" 
           href="/opensearch.xml"
         />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
